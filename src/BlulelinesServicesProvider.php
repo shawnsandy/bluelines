5b56fb15e5b58abba7d845af05897e996a85a588
shawn sandy <shawnsandy04@gmail.com>
2017-05-30 11:28:28 -0400

    Setup - run prefill and rename files

---
+++ src/BlulelinesServicesProvider.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace ShawnSandy\Bluelines;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class Provider
+ * @package ShawnSandy\Bluelines
+ */
+class BluelinesServicesProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+
+        /**
+         * Package views
+         */
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'bluelines');
+        $this->publishes(
+            [
+                __DIR__ . '/resources/views' => resource_path('views/vendor/bluelines'),
+            ], 'bluelines-views'
+        );
+
+        /**
+         * Package assets
+         */
+        $this->publishes(
+            [
+                __DIR__.'/resources/assets/js/' => public_path('assets/bluelines/js/'),
+                __DIR__.'/public/assets/' => public_path('assets/')
+            ], 'bluelines-assets'
+        );
+
+        /**
+         * Package config
+         */
+        $this->publishes(
+            [__DIR__ . '/config/config.php' => config_path('bluelines.php')],
+            'bluelines-config'
+        );
+
+        if (!$this->app->runningInConsole()) :
+            include_once __DIR__ . '/Helpers/helper.php';
+        endif;
+
+
+
+
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+       $this->mergeConfigFrom(
+            __DIR__ . '/config/config.php', 'bluelines'
+        );
+
+        $this->app->bind(
+        'Bluelines', function () {
+        return new Bluelines();
+        }
+        );
+    }
+
+
+}
