381384a8b03fccca997d46a1b499693004b89e2c
shawn sandy <shawnsandy04@gmail.com>
2017-05-31 15:26:43 -0400

    FIX - add missing page_title

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,11 +1,18 @@
 @extends('bluelines::layouts.layout')
 @section('title', 'bluelines')
+@section('page_title', 'Bluelines')
 @section('content')
     <div class="container">
         <h1>Hello world...</h1>
-        @foreach(DashForms::buildModelFields('App\User') as $field)
-            {{ $field }}
-        @endforeach
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci architecto beatae, deserunt
+            facere facilis fugiat fugit iure iusto laborum, magnam magni perspiciatis, quaerat quia rerum sed totam vero
+            voluptates!</p>
+        <p>Animi eveniet illo inventore natus perferendis qui sapiente velit! Est modi nobis recusandae veritatis. Animi
+            debitis deserunt dolorem earum esse exercitationem, illo, illum impedit quis reiciendis repellat, velit.
+            Quibusdam, vitae!</p>
+        <p>Ad eligendi, illo molestias natus nostrum quasi qui quisquam tempora. Ad consectetur deserunt ducimus eveniet
+            ipsam, nulla quasi repellat. Consectetur dolorum ex fuga necessitatibus nesciunt qui ratione repudiandae
+            tempore ut.</p>
     </div>
 
 @endsection
