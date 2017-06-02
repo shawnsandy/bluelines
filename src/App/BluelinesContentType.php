93fe684cab883e52c9ecb01f4e7f8997a4f06fba
shawnsandy04 <shawnsandy04@gmail.com>
2017-06-01 20:47:26 -0400

    Feature - Add content type model

---
+++ src/App/BluelinesContentType.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Shawn
+     * Date: 6/1/2017
+     * Time: 7:33 PM
+     */
+
+    namespace ShawnSandy\Bluelines\App;
+
+
+    use Illuminate\Database\Eloquent\Model;
+
+    class BluelinesContentType extends Model
+    {
+
+    }
