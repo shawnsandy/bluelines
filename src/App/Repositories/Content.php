3ad8518fd7eb61a605cde860dcc7d80d14b79023
shawnsandy04 <shawnsandy04@gmail.com>
2017-06-14 06:51:04 -0400

    Feature -Add repository pattern for content queries

---
+++ src/App/Repositories/Content.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Shawn
+     * Date: 6/13/2017
+     * Time: 4:06 PM
+     */
+
+    namespace components\bluelines\src\App\Repositories;
+
+
+    class Content
+    {
+
+    }
