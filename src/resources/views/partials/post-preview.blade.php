8a980a781e7448be24ff226b5201f6f860767371
shawn sandy <shawnsandy04@gmail.com>
2017-06-16 15:37:50 -0400

    Feature - add a post preview blade

---
+++ src/resources/views/partials/post-preview.blade.php
@@ -0,0 +1,15 @@
+<p>
+    <img src="{{ Html::postImg($post->featured_image, "?w=820") }}" alt="" class="img-responsive">
+</p>
+
+<h1>
+    {{ $post->title }}
+</h1>
+
+<div class="meta">
+    Posted : {{ $post->created_at->diffForHumans() }}
+</div>
+<hr>
+<p>
+    {!! $post->excerpt !!}
+</p>
