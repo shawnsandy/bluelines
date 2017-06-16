9bcf893f398e82ec780f8b2f38d571320061f9b7
shawn sandy <shawnsandy04@gmail.com>
2017-06-16 16:32:10 -0400

    Refactor - move sync/upload methods from controller to request

--- src/App/Controllers/BluelinesController.php
+++ src/App/Controllers/BluelinesController.php
@@ -9,7 +9,6 @@
 namespace ShawnSandy\Bluelines\App\Controllers;
 
 
-use Carbon\Carbon;
 use Illuminate\Routing\Controller;
 use ShawnSandy\Bluelines\App\Blueline;
 use ShawnSandy\Bluelines\App\Request\BluelineRequest;
@@ -35,25 +34,23 @@ class BluelinesController extends Controller
 
         $data = $request->all();
         $data['author_id'] = 1;
+        $data["featured_image"] = $request->upload();
 
-        if ($request->hasFile("featured_image") && $request->file("featured_image")->isValid()) {
-
-            $data['featured_image'] = $request->file('featured_image')->store('img', 'public');
-
-        }
 
         if ($post = Blueline::create($data)) {
 
-            $this->sync($post);
+            $request->syncRelated($post);
 
-           return redirect("/bluelines/posts/{$post->id}/edit")->with('success', "Your post {$post->title} was  created");
+            $request->createOrUpdateSlug($post);
+
+            return redirect("/bluelines/posts/{$post->id}/edit")->with('success', "Your post {$post->title} was  created");
 
         }
 
-
-       return back()->with('error', "Sorry your post was not saved, please try again.");
+        return back()->with('error', "Sorry your post was not saved, please try again.");
 
     }
+
 
     public function edit($post_id)
     {
@@ -71,17 +68,14 @@ class BluelinesController extends Controller
 //            dd($request->all());
 
         $data = $request->input();
+        $data['featured_image'] = $request->upload();
 
-        if ($request->hasFile("featured_image") && $request->file("featured_image")->isValid()) {
-
-            $data['featured_image'] = $request->file('featured_image')->store('img', 'public');
-
-
-        }
 
         if ($post = Blueline::updateOrCreate(["id" => $post_id], $data)):
 
-            $this->sync($post);
+            $request->syncRelated($post);
+
+            $request->createOrUpdateSlug($post);
 
             return back()->with("success", "Your post has been updated!");
 
@@ -98,25 +92,6 @@ class BluelinesController extends Controller
         endif;
 
         return back()->with("error", "Sorry you post was not deleted!");
-    }
-
-    /**
-     * @param $post
-     */
-    private function sync($post): void
-    {
-
-        if (!request()->has("slug")):
-            $slug = str_slug($post->title . "-" . $post->created_at->toDateString());
-            Blueline::updateOrCreate(["id" => $post->id], ["slug" => $slug]);
-        endif;
-
-        if (request()->has("categories"))
-            $post->categories()->sync(request()->input("categories"));
-
-        if (request()->has("tags"))
-            $post->tags()->sync(request()->input("tags"));
-
     }
 
 
--- src/App/Request/BluelineRequest.php
+++ src/App/Request/BluelineRequest.php
@@ -1,36 +1,71 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: Shawn
-     * Date: 6/2/2017
-     * Time: 8:45 PM
-     */
+/**
+ * Created by PhpStorm.
+ * User: Shawn
+ * Date: 6/2/2017
+ * Time: 8:45 PM
+ */
 
-    namespace ShawnSandy\Bluelines\App\Request;
+namespace ShawnSandy\Bluelines\App\Request;
 
 
-    use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Foundation\Http\FormRequest;
+use ShawnSandy\Bluelines\App\Blueline;
 
-    class BluelineRequest extends FormRequest
+class BluelineRequest extends FormRequest
+{
+
+    public function authorize()
     {
-
-        public function authorize()
-        {
-            return true;
-
-        }
-
-        public function rules()
-        {
-
-            return [
-                "title" => "required|max:200",
-                "slug" => "max:200",
-                "body" => "required|min:50",
-                "featured_image" => "image",
-                "category" => "sometimes|required"
-            ];
-
-        }
+        return true;
 
     }
+
+    public function rules()
+    {
+
+        return [
+            "title" => "required|max:200",
+            "slug" => "max:200",
+            "body" => "required|min:50",
+            "featured_image" => "image",
+            "category" => "sometimes|required"
+        ];
+
+    }
+
+    /**
+     * Sync /  attach all related fields to pivot tables
+     *
+     * @param $post
+     */
+    public function syncRelated($post)
+    {
+
+        if ($this->has("categories"))
+            $post->categories()->sync(request()->input("categories"));
+
+        if ($this->has("tags"))
+            $post->tags()->sync(request()->input("tags"));
+
+    }
+
+    public function createOrUpdateSlug($post)
+    {
+
+        if (!$this->has("slug")):
+            $slug = str_slug($post->title . "-" . $post->created_at->toDateTimeString());
+            Blueline::updateOrCreate(["id" => $post->id], ["slug" => $slug]);
+        endif;
+    }
+
+    public function upload() {
+
+        if ($this->hasFile("featured_image") && $this->file("featured_image")->isValid()) {
+
+            return $this->file('featured_image')->store('img', 'public');
+
+        }
+    }
+
+}
