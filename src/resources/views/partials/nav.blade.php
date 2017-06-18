b3061bbeb995aa189aba67e0f1a071205bf74e42
shawnsandy04 <shawnsandy04@gmail.com>
2017-06-18 12:43:06 -0400

    Update - remove create page button

--- src/resources/views/partials/nav.blade.php
+++ src/resources/views/partials/nav.blade.php
@@ -1,5 +1,4 @@
 <div class="h1">
     <a href="/bluelines/posts/create" class="btn btn-default btn-primary">Create A New Post</a>
-    <a href="/bluelines/posts/create" class="btn btn-default btn-primary">Create A New Page</a>
-    <a href="/bluelines/" class="btn btn-default">Settings</a>
+    <a href="/bluelines/settings" class="btn btn-default">Settings</a>
 </div>
