149e84f430eb3cfdf54108a5a3b70d78946cf6ba
shawn sandy <shawnsandy04@gmail.com>
2017-06-20 16:24:26 -0400

    WIP - ckeditor file uploads

--- src/resources/views/components/ckeditor.blade.php
+++ src/resources/views/components/ckeditor.blade.php
@@ -1,6 +1,9 @@
 @push("scripts")
 <script src="//cdn.ckeditor.com/4.7.0/standard/ckeditor.js"></script>
 <script>
-    CKEDITOR.replace('body');
+    CKEDITOR.replace('body', {
+        filebrowserBrowseUrl: '/browser/browse.php',
+        filebrowserUploadUrl: '/uploader/upload.php'
+    });
 </script>
 @endpush
