e66197f269e0bb895e72b8c36c70d2df4cf10154
shawnsandy04 <shawnsandy04@gmail.com>
2017-08-04 20:43:29 -0400

    Updates - remove title "recent post"

--- src/resources/views/components/recent-content.blade.php
+++ src/resources/views/components/recent-content.blade.php
@@ -2,9 +2,6 @@
     $recent_posts = Blue::recentPostsUpdates(7);
 @endphp
 <ul class="list-group">
-    <li class="list-group-item">
-        <h4>Recent Post</h4>
-    </li>
     @if(count($recent_posts))
         @foreach($recent_posts as $post_item)
             <li class="list-group-item">
