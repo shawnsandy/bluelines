5b076abfcf03c0ae601d2aa6fb86af5bd3433396
shawnsandy04 <shawnsandy04@gmail.com>
2017-08-17 14:05:16 -0400

    Update - add tinymce, remove inline js, use tinymce component

--- src/resources/views/components/ckeditor.blade.php
+++ src/resources/views/components/ckeditor.blade.php
@@ -1,12 +1,13 @@
 @push("scripts")
 <script src="//cdn.ckeditor.com/4.7.0/standard/ckeditor.js"></script>
 <script>
+    var editor = "{{ $editor or 'body' }}";
     var options = {
         filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
         filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
         filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
         filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
     };
-    CKEDITOR.replace('body', options);
+    CKEDITOR.replace(editor, options);
 </script>
 @endpush
--- src/resources/views/partials/forms/post.blade.php
+++ src/resources/views/partials/forms/post.blade.php
@@ -81,15 +81,6 @@
         tags: true
     });
 </script>
-<script src="//cdn.ckeditor.com/4.7.0/standard/ckeditor.js"></script>
-<script>
-    var options = {
-        filebrowserImageBrowseUrl: '/laravel-filemanager?type=Images',
-        filebrowserImageUploadUrl: '/laravel-filemanager/upload?type=Images&_token=',
-        filebrowserBrowseUrl: '/laravel-filemanager?type=Files',
-        filebrowserUploadUrl: '/laravel-filemanager/upload?type=Files&_token='
-    };
-    CKEDITOR.replace('body', options);
-</script>
+
 @endpush
 
