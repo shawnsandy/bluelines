a9a513a5bc88bc76125fb0652c137a488f9c1deb
shawn sandy <shawnsandy04@gmail.com>
2017-08-24 14:06:42 -0400

    Update : add somtmes to image validatation

--- src/App/Request/BluelineFormRequest.php
+++ src/App/Request/BluelineFormRequest.php
@@ -29,7 +29,7 @@
                 "title" => "required|max:200",
                 "slug" => "max:200",
                 "body" => "required|min:50",
-                "featured_image" => "image",
+                "featured_image" => "sometimes|image",
                 "category" => "sometimes|required"
             ];
 
